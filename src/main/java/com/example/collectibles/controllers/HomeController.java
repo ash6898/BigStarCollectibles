76de7db46c1e63712adf3fc3a7bcb438f4f031ac
Aakash Baskaran <aakashguru6898@gmail.com>
2026-02-13 15:07:31 -0500

    Setted up the Collectibles App

---
+++ src/main/java/com/example/collectibles/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.collectibles.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/home")
+    public String displayHome() {
+        return "index";
+    }
+}
