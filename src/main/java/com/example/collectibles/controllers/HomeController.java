46d7494ce40549d687b34cf854ffcd3ab411cd77
Aakash Baskaran <aakashguru6898@gmail.com>
2026-02-14 09:37:39 -0500

    fragmented the header and footer as modular components

--- src/main/java/com/example/collectibles/controllers/HomeController.java
+++ src/main/java/com/example/collectibles/controllers/HomeController.java
@@ -2,6 +2,8 @@ package com.example.collectibles.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @Controller
 public class HomeController {
@@ -9,5 +11,10 @@ public class HomeController {
     @GetMapping("/home")
     public String displayHome() {
         return "index";
+    }
+
+    @GetMapping("/getcharacter/{charname}")
+    public String getCharacter(@PathVariable("charname") String charName) {
+        return "/characters/" + charName;
     }
 }
